4112d149546781aca395b0e1f4da8b601bd5c01a
Andrew <andrewluferenko@icloud.com>
2023-01-11 22:22:40 +0300

    after fixed bugs

--- Lesson 1/GuessNumber.java
+++ Lesson 1/GuessNumber.java
@@ -3,48 +3,19 @@ public class GuessNumber {
         int guessNumber = 10;
         int min = 0;
         int max = 100;
-        int playerNumber = max / 2;
+        int playerNumber = 1;
 
         if (guessNumber > min && guessNumber <= max) {
             while (true) {
                 if (playerNumber < guessNumber) {
                     System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
-                    playerNumber += playerNumber / 2;
-                    if (playerNumber < guessNumber) {
-                        System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
-                        while (playerNumber != guessNumber) {
-                            System.out.println("Число " + ++playerNumber + " меньше того, что загадал компьютер");
-                        }
-                    }
-                    if (playerNumber > guessNumber) {
-                        System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
-                        while (playerNumber != guessNumber) {
-                            System.out.println("Число " + --playerNumber + " больше того, что загадал компьютер");
-                        }
-                    }
-                    if (playerNumber == guessNumber) {
-                        System.out.println("Вы победили!");
-                        break;
-                    }
+                    playerNumber++;
                 } else if (playerNumber > guessNumber) {
                     System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
-                    playerNumber -= playerNumber / 2;
-                    if (playerNumber < guessNumber) {
-                        System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
-                        while (playerNumber != guessNumber) {
-                            System.out.println("Число " + ++playerNumber + " меньше того, что загадал компьютер");
-                        }
-                    }
-                    if (playerNumber > guessNumber) {
-                        System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
-                        while (playerNumber != guessNumber) {
-                            System.out.println("Число " + --playerNumber + " больше того, что загадал компьютер");
-                        }
-                    }
-                    if (playerNumber == guessNumber) {
-                        System.out.println("Вы победили!");
-                        break;
-                    }
+                    playerNumber--;
+                } else if (playerNumber == guessNumber) {
+                    System.out.println("Вы победили!");
+                    break;
                 }
             }
         }

f7e2cff49245bed916b4a1de035e8bcdcfcc4322
Andrew <andrewluferenko@icloud.com>
2023-01-13 09:48:38 +0300

    after fixed bugs

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,12 +1,9 @@
 public class Calculator {
     public static void main(String[] args) {
-        int num1 = 4;
-        int num2 = 14;
-        int powValue = 3;
-        char sign = '^';
+        int num1 = 3;
+        int num2 = 3;
+        char sign = '-';
         int result = 0;
-        int count = 3;
-        int resultPow = 1;
 
         if (sign == '+') {
             result = num1 + num2;
@@ -18,16 +15,12 @@ public class Calculator {
             result = num1 / num2;
         } else if (sign == '%') {
             result = num1 % num2;
-        }
-
-        if (sign == '^') {
-            while (count != 0) {
-                resultPow = resultPow * num1;
-                count--;
+        } else if (sign == '^') {
+            for (int i = 1; i <= num2; i++) {
+                result = 1;
+                result = result * num1;
             }
-            System.out.println(num1 + " " + sign + ' ' + powValue + " " + " = " + resultPow);
-        } else {
-            System.out.println(num1 + " " + sign + ' ' + num2 + " " + " = " + result);
         }
+        System.out.println(num1 + " " + sign + ' ' + num2 + " " + " = " + result);
     }
 }

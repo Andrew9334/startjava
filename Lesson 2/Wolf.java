e776a59e6786f48b1556dda96a2af92c7aa365da
Andrew <andrewluferenko@icloud.com>
2023-01-18 20:53:30 +0300

    for check

--- Lesson 2/Wolf.java
+++ Lesson 2/Wolf.java
@@ -1,10 +1,53 @@
 public class Wolf {
-    char sex;
-    String nickname;
-    int weight;
-    int age;
-    String color;
+    private char sex;
+    private String nickname;
+    private int weight;
+    private int age;
+    private String color;
 
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        }
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    
     void walk() {
         System.out.println("Идет");
     }

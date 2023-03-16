fd88ee74b05b30be68c305248dbd3aabc164d7b5
Andrew9334 <96207715+Andrew9334@users.noreply.github.com>
2023-03-16 22:47:06 +0300

    after fixed bugs

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,15 +1,16 @@
 CREATE DATABASE jaegers;
 
 CREATE TABLE jaegers (
-id SERIAL PRIMARY KEY NOT NULL,
-modelName TEXT,
-mark TEXT,
-height INTEGER NOT null, 
-weight INTEGER NOT null,
-status TEXT,
-origin TEXT, 
-launch TEXT,
-kaijuKill INTEGER);
+	id SERIAL PRIMARY KEY NOT NULL,
+	modelName TEXT,
+	mark TEXT,
+	height INTEGER NOT null, 
+	weight INTEGER NOT null,
+	status TEXT,
+	origin TEXT, 
+	launch TEXT,
+	kaijuKill INTEGER
+);
 
 \i init_db.sql;
 \i queries.sql;
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,29 +1,55 @@
-\echo '--вывод всей таблицы'
-SELECT * FROM jaegers;
+﻿\echo '--вывод всей таблицы'
+SELECT * 
+  FROM jaegers;
 
 \echo '--вывод только не уничтоженных роботов'
-SELECT * FROM jaegers WHERE status NOT IN ('Destroyed');
+SELECT * 
+  FROM jaegers 
+ WHERE status != ('Destroyed');
  
 \echo '--вывод роботов серий: Mark-1 и Mark-4'
-SELECT * FROM jaegers WHERE mark IN ('Mark-1', 'Mark-4');  
+SELECT * 
+  FROM jaegers 
+ WHERE mark 
+    IN ('Mark-1', 'Mark-4');  
 
 \echo '--вывод всех роботов, кроме Mark-1 и Mark-4'
-SELECT * FROM jaegers WHERE mark NOT IN ('Mark-1', 'Mark-4'); 
+SELECT * 
+  FROM jaegers 
+ WHERE mark 
+NOT IN ('Mark-1', 'Mark-4'); 
 
 \echo '--сортировка таблицы по убыванию'
-SELECT * FROM jaegers ORDER BY mark DESC; 
+  SELECT * 
+    FROM jaegers 
+ORDER BY mark 
+    DESC; 
 
 \echo '--информация о самом старом роботе'
-SELECT * FROM jaegers WHERE launch = (SELECT MIN(launch) FROM jaegers);
+SELECT * 
+  FROM jaegers 
+ WHERE launch = (SELECT MIN(launch) 
+  		   FROM jaegers
+		);
 
 \echo '--роботы, которые унитожили больше всех kaiju'
-SELECT * FROM jaegers WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM jaegers); 
+SELECT * 
+  FROM jaegers 
+ WHERE kaijuKill = (SELECT MAX(kaijuKill) 
+  		      FROM jaegers
+		   ); 
 
 \echo '--средний вес роботов'
-SELECT AVG(weight) from jaegers; 
+SELECT 
+   AVG(weight) 
+  FROM jaegers; 
 
 \echo '--увеличение на единицу количества уничтоженных kaiju у роботов, которые до сих пор не разрушены'
-UPDATE jaegers SET kaijuKill = kaijuKill+1 WHERE status IN ('Active'); 
+UPDATE jaegers 
+   SET kaijuKill = kaijuKill + 1 
+ WHERE status = 'Active'; 
 
 \echo '--удаление уничтоженных роботов'
-DELETE FROM jaegers WHERE status IN ('Destroyed'); 
+DELETE 
+  FROM jaegers 
+ WHERE status = 'Destroyed'; 

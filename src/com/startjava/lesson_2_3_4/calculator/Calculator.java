0fb58ece1987b7c2a34b35bf6b95144c4ab1ca8d
Andrew9334 <96207715+Andrew9334@users.noreply.github.com>
2023-03-19 14:49:20 +0300

    for check after update

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,35 +1,34 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    private int num1;
-    private int num2;
-    private char sign;
+    private static int num1;
+    private static int num2;
+    private static char sign;
 
-    public double calculate(String expression) {
+    public static double calculate(String expression) {
         parseExpression(expression);
-        switch (sign) {
-            case '+':
-                return Math.addExact(num1, num2);
-            case '-':
-                return Math.subtractExact(num1, num2);
-            case '*':
-                return Math.multiplyExact(num1, num2);
-            case '^':
-                return Math.pow(num1, num2);
-            case '/':
-                return (double) num1 / num2;
-            case '%':
-                return Math.floorMod(num1, num2);
-            default:
-                System.out.println("Знак " + sign + " не поддерживается");
-        }
-        return 0;
+        return switch (sign) {
+            case '+' -> Math.addExact(num1, num2);
+            case '-' -> Math.subtractExact(num1, num2);
+            case '*' -> Math.multiplyExact(num1, num2);
+            case '^' -> Math.pow(num1, num2);
+            case '/' -> Math.floorDiv(num1, num2);
+            case '%' -> Math.floorMod(num1, num2);
+            default -> 0;
+        };
     }
 
-    private void parseExpression(String expression) {
+    private static void parseExpression(String expression) {
         String[] partsExpression = expression.split(" ");
-        num1 = Integer.parseInt(partsExpression[0]);
+        try {
+            if ((num1 % 1 == 0.0 && num2 % 1 == 0.0) || (num1 < 0 || num2 < 0)) {
+                num1 = Integer.parseInt(partsExpression[0]);
+                num2 = Integer.parseInt(partsExpression[2]);
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Введите натуральное число");
+        }
         sign = partsExpression[1].charAt(0);
-        num2 = Integer.parseInt(partsExpression[2]);
     }
 }

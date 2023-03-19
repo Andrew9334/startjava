a8d6a9d4eaa14ccbfe5a857716c8092e70f3bdff
Andrew9334 <96207715+Andrew9334@users.noreply.github.com>
2023-03-19 14:52:28 +0300

    for check after update

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,16 +1,16 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
+import static com.startjava.lesson_2_3_4.calculator.Calculator.calculate;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         String answer = "yes";
         do {
             if ("yes".equals(answer)) {
                 System.out.print("Введите математическое выражение: ");
-                printResult(calculator.calculate(scanner.nextLine()));
+                printResult(calculate(scanner.nextLine()));
             }
             System.out.println("Хотите продолжить вычисления? [yes/no]");
             answer = scanner.nextLine();
